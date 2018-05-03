d1a8f77281fde2b76fe552acd9940c1a08721f99
Ben Moon <software@guiltydolphin.com>
2018-05-03 11:16:43 +0100

    Release version 0.1.0
    This has no functionality yet, but adds in necessary licensing, as well
    as other skeleton files.

---
+++ examr.el
@@ -0,0 +1,35 @@
+;;; examr.el --- Helpers for working with exam resources.
+
+;; Copyright (C) 2018 Ben Moon
+;; Author: Ben Moon <software@guiltydolphin.com>
+;; URL: https://github.com/guiltydolphin/examr
+;; Git-Repository: git://github.com/guiltydolphin/examr.git
+;; Created: 2018-05-03
+;; Version: 0.1.0
+;; Keywords: files, outlines
+;; Package-Requires: ((dash "2.13.0"))
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; examr provides utilities for parsing, annotating, and
+;; working with various exam and revision resources.
+;;
+;; For more information see the README.
+
+;;; Code:
+
+(provide 'examr)
+;;; examr.el ends here
